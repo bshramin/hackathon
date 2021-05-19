6de6d920a8b6da84cfd043e656e592bd02af6f6b
Amin Bashiri <amin.bashiri28@gmail.com>
2021-05-19 18:45:01 +0430

    counts

--- do.py
+++ do.py
@@ -8,9 +8,12 @@ import time
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
+number_of_regions = []
+start_time = time.time()
 
 while True:
     response = requests.get(url="http://esp-WiFi.local:80/capture")
+        
     img_str = response.content
 
     nparr = np.frombuffer(img_str, np.uint8)
@@ -34,6 +37,7 @@ while True:
 
         # Drawing the regions in the
         # Image
+        number_of_regions.append(len(regions))
         for (x, y, w, h) in regions:
             cv2.rectangle(
                 image,
@@ -45,7 +49,17 @@ while True:
 
         # Showing the output Image
         cv2.imshow("Image", image)
+        
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
+
+        if int(time.time() - start_time) >= 10:
+            if number_of_regions == 0:
+                print(f"no data in {int(time.time() - start_time)} seconds")
+                continue
+            print(round(sum(number_of_regions)/len(number_of_regions)))
+            number_of_regions = []
+            start_time = time.time()
+        
     else:
         break
